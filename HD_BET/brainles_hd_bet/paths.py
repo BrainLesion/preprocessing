68314c492d887d4aa20ee42fdc02b831888cd694
neuronflow <florian.kofler@tum.de>
2023-08-01 10:43:43 +0200

    new: init hd_bet
    Signed-off-by: neuronflow <florian.kofler@tum.de>

---
+++ HD_BET/brainles_hd_bet/paths.py
@@ -0,0 +1,5 @@
+import os
+
+# please refer to the readme on where to get the parameters. Save them in this folder:
+# folder_with_parameter_files = os.path.join(os.path.expanduser('~'), 'hd-bet_params')
+folder_with_parameter_files = "HD-BET_model-weights"
