19d3c20e7047f27fc3ae4d50fcdbf30aca39c6cd
neuronflow <florian.kofler@tum.de>
2023-08-13 20:58:34 +0200

    hdbet
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- preprocessing/brainles_preprocessing/brain_extraction/hdbet.py
+++ preprocessing/brainles_preprocessing/brain_extraction/hdbet.py
@@ -1,89 +1,27 @@
-import pathlib
-import shlex
-import datetime
-from ttictoc import Timer
-import subprocess
+from brainles_hd_bet import run_hd_bet
 
-
-import os
+TODO = "TODO"
 
 
 def hdbet_caller(
     input_image,
     masked_image,
+    # TODO implement logging!
     log_file,
     mode,
 ):
+    # GPU + accurate + TTA
     """skullstrips images with HD-BET generates a skullstripped file and mask"""
-    the_shell = "/bin/bash"
-    brain_extraction_abspath = os.path.dirname(os.path.abspath(__file__))
-
-    if mode == "gpu":
-        shell_script = os.path.join(
-            brain_extraction_abspath, "hdbet_scripts", "hd-bet_gpu.sh"
-        )
-    elif mode == "cpu":
-        shell_script = os.path.join(
-            brain_extraction_abspath, "hdbet_scripts", "hd-bet_cpu.sh"
-        )
-    elif mode == "cpu-fast":
-        shell_script = os.path.join(
-            brain_extraction_abspath, "hdbet_scripts", "hd-bet_cpu-fast.sh"
-        )
-    else:
-        raise NotImplementedError("this mode is not implemented:", mode)
-    # let's try to call it
-    try:
-        starttime = str(datetime.datetime.now())
-        print(
-            "** starting skullstripping with:",
-            mode,
-            "for:",
-            input_image.name,
-            "at:",
-            starttime,
-        )
-        t = Timer()  # TicToc("name")
-        t.start()
-
-        # generate subprocess call
-        readableCmd = (
-            the_shell,
-            shell_script,
-            input_image,
-            masked_image,
-        )
-        readableCmd = " ".join(readableCmd)
-        print(readableCmd)
-        command = shlex.split(readableCmd)
-        print(command)
-
-        # cwd = pathlib.Path(__file__).resolve().parent
-        cwd = brain_extraction_abspath
-        print(cwd)
-
-        with open(log_file, "w") as outfile:
-            subprocess.run(command, stdout=outfile, stderr=outfile, cwd=cwd)
-
-        endtime = str(datetime.datetime.now().time())
-
-        elapsed = t.stop("call")
-        print(elapsed)
-
-        with open(log_file, "a") as file:
-            file.write("\n" + "************************************************" + "\n")
-            file.write("CALL: " + readableCmd + "\n")
-            file.write("cwd: " + str(cwd) + "\n")
-            file.write("************************************************" + "\n")
-            file.write("************************************************" + "\n")
-            file.write("start time: " + starttime + "\n")
-            file.write("end time: " + endtime + "\n")
-            file.write("time elapsed: " + str(int(elapsed) / 60) + " minutes" + "\n")
-            file.write("************************************************" + "\n")
-
-    except Exception as e:
-        print("error: " + str(e))
-        print("skullstripping error for: " + input_image.name)
-
-    endtime = str(datetime.datetime.now())
-    print("** finished: " + input_image.name + " at: " + endtime)
+    run_hd_bet(
+        mri_fnames=[input_image],
+        output_fnames=[masked_image],
+        # device=0,
+        # TODO consider postprocessing
+        # postprocess=False,
+        mode="accurate",
+        device=0,
+        postprocess=False,
+        do_tta=True,
+        keep_mask=True,
+        overwrite=True,
+    )
