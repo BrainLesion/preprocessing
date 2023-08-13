d6ddf0a23a3f8a8e580f1fb69005ab52462889a2
neuronflow <florian.kofler@tum.de>
2023-08-13 23:58:26 +0200

    the Registrator
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- preprocessing/brainles_preprocessing/registration/registration.py
+++ preprocessing/brainles_preprocessing/registration/registration.py
@@ -1,7 +1,18 @@
 from .niftyreg import niftyreg_caller
 
 
-def registrator(
+class Registrator:
+    def __init__(self, backend="niftyreg"):
+        self.backend = backend
+
+    def register(self, fixed_image, moving_image, transformed_image, matrix, log_file):
+        raise NotImplementedError("Subclasses must implement the register method")
+
+    def transform(self, fixed_image, moving_image, transformed_image, matrix, log_file):
+        raise NotImplementedError("Subclasses must implement the transform method")
+
+
+def register(
     fixed_image,
     moving_image,
     transformed_image,
