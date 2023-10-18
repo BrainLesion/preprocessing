94923640d4ca67ab76b9f4cf3a8c0d7048eb06b7
neuronflow <florian.kofler@tum.de>
2023-10-18 04:52:20 +0200

    commented out test case
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- preprocessing/test_preprocessing_brats.py
+++ preprocessing/test_preprocessing_brats.py
@@ -23,39 +23,39 @@ def preprocess(inputDir):
         prep_dir = brainles_dir + "/preprocessed"
 
         # if not os.path.exists(prep_dir):
-        if os.path.exists(prep_dir):
-            t1_file = btk_raw_dir.files("*t1.nii.gz")
-            t1c_file = btk_raw_dir.files("*t1c.nii.gz")
-            t2_file = btk_raw_dir.files("*t2.nii.gz")
-            flair_file = btk_raw_dir.files("*fla.nii.gz")
+        # if os.path.exists(prep_dir):
+        t1_file = btk_raw_dir.files("*t1.nii.gz")
+        t1c_file = btk_raw_dir.files("*t1c.nii.gz")
+        t2_file = btk_raw_dir.files("*t2.nii.gz")
+        flair_file = btk_raw_dir.files("*fla.nii.gz")
 
-            if len(t1_file) == len(t1c_file) == len(t2_file) == len(flair_file) == 1:
-                print(t1_file)
-                print(t1c_file)
-                print(t2_file)
-                print(flair_file)
+        if len(t1_file) == len(t1c_file) == len(t2_file) == len(flair_file) == 1:
+            print(t1_file)
+            print(t1c_file)
+            print(t2_file)
+            print(flair_file)
 
-                t1File = t1_file[0]
-                t1cFile = t1c_file[0]
-                t2File = t2_file[0]
-                flaFile = flair_file[0]
+            t1File = t1_file[0]
+            t1cFile = t1c_file[0]
+            t2File = t2_file[0]
+            flaFile = flair_file[0]
 
-                # execute it
-                preprocess_brats_style_t1_centric(
-                    input_t1=t1File,
-                    output_t1=prep_dir + "/" + inputDir.name + "_t1.nii.gz",
-                    input_t1c=t1cFile,
-                    output_t1c=prep_dir + "/" + inputDir.name + "_t1c.nii.gz",
-                    input_t2=t2File,
-                    output_t2=prep_dir + "/" + inputDir.name + "_t2.nii.gz",
-                    input_flair=flaFile,
-                    output_flair=prep_dir + "/" + inputDir.name + "_fla.nii.gz",
-                    bet_mode="gpu",
-                    limit_cuda_visible_devices="0",
-                    keep_coregistration=brainles_dir + "/co-registration",
-                    keep_atlas_registration=brainles_dir + "/atlas-registration",
-                    keep_brainextraction=brainles_dir + "/brain-extraction",
-                )
+            # execute it
+            preprocess_brats_style_t1_centric(
+                input_t1=t1File,
+                output_t1=prep_dir + "/" + inputDir.name + "_t1.nii.gz",
+                input_t1c=t1cFile,
+                output_t1c=prep_dir + "/" + inputDir.name + "_t1c.nii.gz",
+                input_t2=t2File,
+                output_t2=prep_dir + "/" + inputDir.name + "_t2.nii.gz",
+                input_flair=flaFile,
+                output_flair=prep_dir + "/" + inputDir.name + "_fla.nii.gz",
+                bet_mode="gpu",
+                limit_cuda_visible_devices="0",
+                keep_coregistration=brainles_dir + "/co-registration",
+                keep_atlas_registration=brainles_dir + "/atlas-registration",
+                keep_brainextraction=brainles_dir + "/brain-extraction",
+            )
 
     except Exception as e:
         print("error: " + str(e))
