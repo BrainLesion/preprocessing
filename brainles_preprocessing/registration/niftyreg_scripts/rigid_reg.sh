631cd5fe1814d4f7588548a102f5140ab5d40de8
neuronflow <florian.kofler@tum.de>
2023-10-27 01:52:07 +0200

    nicer scripts
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- brainles_preprocessing/registration/niftyreg_scripts/rigid_reg.sh
+++ brainles_preprocessing/registration/niftyreg_scripts/rigid_reg.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
+# Function to check if a file exists
+file_exists() {
+    if [ -f "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 4 ]; then
     echo "Usage: $0 <fixed_image> <moving_image> <transformed_image> <transformation_matrix>"
     exit 1
 fi
 
-# Assign arguments to variables
-fixed="$1"
-moving="$2"
-transformed="$3"
-matrix="$4"
+# Assign arguments to meaningful variable names
+fixed_image="$1"
+moving_image="$2"
+transformed_image="$3"
+transformation_matrix="$4"
 
-# Check if input files exist
-for file in "$fixed" "$moving"; do
-    if [ ! -f "$file" ]; then
-        echo "Error: File '$file' does not exist."
-        exit 1
-    fi
-done
+# Validate the existence of input files
+if ! file_exists "$fixed_image"; then
+    echo "Error: Fixed image '$fixed_image' does not exist."
+    exit 1
+fi
+
+if ! file_exists "$moving_image"; then
+    echo "Error: Moving image '$moving_image' does not exist."
+    exit 1
+fi
+
+# NiftyReg configuration
+niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_aladin"
+registration_options=(
+    "-rigOnly"     # Perform rigid-only registration
+    "-ref" "$fixed_image"
+    "-flo" "$moving_image"
+    "-res" "$transformed_image"
+    "-aff" "$transformation_matrix"
+)
 
 # Perform rigid-only registration with NiftyReg
-niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_aladin"
-affine_options=("-rigOnly" \
-    "-ref" "$fixed" \
-    "-flo" "$moving" \
-    "-res" "$transformed" \
-    "-aff" "$matrix")
-
-if [ -f "$niftyreg_path" ]; then
-    "$niftyreg_path" "${affine_options[@]}"
+if file_exists "$niftyreg_path"; then
+    "$niftyreg_path" "${registration_options[@]}"
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
         echo "Error: NiftyReg registration failed with exit code $exit_code."
--- brainles_preprocessing/registration/niftyreg_scripts/transform.sh
+++ brainles_preprocessing/registration/niftyreg_scripts/transform.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
+# Function to check if a file exists
+file_exists() {
+    if [ -f "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 4 ]; then
     echo "Usage: $0 <fixed_image> <moving_image> <transformed_image> <transformation_matrix>"
     exit 1
 fi
 
-# Assign arguments to variables
-fixed="$1"
-moving="$2"
-transformed="$3"
-matrix="$4"
+# Assign arguments to meaningful variable names
+fixed_image="$1"
+moving_image="$2"
+transformed_image="$3"
+transformation_matrix="$4"
 
-# Check if input files exist
-for file in "$fixed" "$moving" "$matrix"; do
-    if [ ! -f "$file" ]; then
-        echo "Error: File '$file' does not exist."
-        exit 1
-    fi
-done
+# Validate the existence of input files
+if ! file_exists "$fixed_image"; then
+    echo "Error: Fixed image '$fixed_image' does not exist."
+    exit 1
+fi
+
+if ! file_exists "$moving_image"; then
+    echo "Error: Moving image '$moving_image' does not exist."
+    exit 1
+fi
+
+if ! file_exists "$transformation_matrix"; then
+    echo "Error: Transformation matrix file '$transformation_matrix' does not exist."
+    exit 1
+fi
+
+# NiftyReg configuration
+niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_resample"
+interpolation_method="3"  # Choose the appropriate interpolation method (e.g., 0, 1, 3)
 
 # Perform resampling with NiftyReg
-niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_resample"
-resample_options=(
-    "-ref" "$fixed"
-    "-flo" "$moving"
-    "-trans" "$matrix"
-    "-res" "$transformed"
-    "-inter 3"
+resample_command=(
+    "$niftyreg_path"
+    -ref "$fixed_image"
+    -flo "$moving_image"
+    -trans "$transformation_matrix"
+    -res "$transformed_image"
+    -inter "$interpolation_method"
 )
 
-if [ -f "$niftyreg_path" ]; then
-    "$niftyreg_path" "${resample_options[@]}"
+if file_exists "$niftyreg_path"; then
+    "${resample_command[@]}"
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
         echo "Error: NiftyReg failed with exit code $exit_code."
