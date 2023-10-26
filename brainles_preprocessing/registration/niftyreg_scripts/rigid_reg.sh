bdda311dd242afbb8fe42de44a5a399187abe029
neuronflow <florian.kofler@tum.de>
2023-10-27 01:34:57 +0200

    make niftyreg scripts handle special characters
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- brainles_preprocessing/registration/niftyreg_scripts/rigid_reg.sh
+++ brainles_preprocessing/registration/niftyreg_scripts/rigid_reg.sh
@@ -1,20 +1,45 @@
-# rigid only registration with niftyreg
-echo "fixed image: $1"
-echo "moving image: $2"
-echo "transformed image: $3"
-echo "transformation matrix: $4"
+#!/bin/bash
 
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <fixed_image> <moving_image> <transformed_image> <transformation_matrix>"
+    exit 1
+fi
+
+# Assign arguments to variables
 fixed="$1"
 moving="$2"
 transformed="$3"
 matrix="$4"
 
-# niftyreg_scripts/reg_aladin \
-# TODO this path will be an issue for packaging
-brainles_preprocessing/registration/niftyreg_scripts/reg_aladin \
-    -rigOnly \
-    -ref $fixed \
-    -flo $moving \
-    -res $transformed \
-    -aff $matrix
-    # -pad 0
+# Check if input files exist
+for file in "$fixed" "$moving"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: File '$file' does not exist."
+        exit 1
+    fi
+done
+
+# Perform rigid-only registration with NiftyReg
+niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_aladin"
+affine_options=("-rigOnly" \
+    "-ref" "$fixed" \
+    "-flo" "$moving" \
+    "-res" "$transformed" \
+    "-aff" "$matrix")
+
+if [ -f "$niftyreg_path" ]; then
+    "$niftyreg_path" "${affine_options[@]}"
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error: NiftyReg registration failed with exit code $exit_code."
+        exit $exit_code
+    fi
+else
+    echo "Error: NiftyReg script not found at '$niftyreg_path'."
+    exit 1
+fi
+
+# Optional: Additional commands or post-processing steps
+
+echo "Registration completed successfully."
--- brainles_preprocessing/registration/niftyreg_scripts/transform.sh
+++ brainles_preprocessing/registration/niftyreg_scripts/transform.sh
@@ -1,21 +1,41 @@
-# "reg_resample -ref " + ordner + pat + "/t1.nii.gz -flo " + ordner + pat + "/f2.nii.gz -trans " + ordner + pat + "/atlasreg.txt -res " + ordner + pat + "/f2.nii.gz -pad 0 -inter 3"
-# rigid only registration with niftyreg
-echo "fixed image: $1"
-echo "moving image: $2"
-echo "transformed image: $3"
-echo "transformation matrix: $4"
+#!/bin/bash
 
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <fixed_image> <moving_image> <transformation_matrix> <transformed_image>"
+    exit 1
+fi
+
+# Assign arguments to variables
 fixed="$1"
 moving="$2"
-transformed="$3"
-matrix="$4"
+matrix="$3"
+transformed="$4"
 
-# niftyreg_scripts/reg_resample \
-# TODO this path will be an issue for packaging
-brainles_preprocessing/registration/niftyreg_scripts/reg_resample \
-    -ref $fixed \
-    -flo $moving \
-    -trans $matrix \
-    -res $transformed \
-    -inter 3
-    # -pad 0
+# Check if input files exist
+for file in "$fixed" "$moving" "$matrix"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: File '$file' does not exist."
+        exit 1
+    fi
+done
+
+# Perform resampling with NiftyReg
+niftyreg_path="brainles_preprocessing/registration/niftyreg_scripts/reg_resample"
+resample_options=("-ref" "$fixed" "-flo" "$moving" "-trans" "$matrix" "-res" "$transformed" "-inter 3")
+
+if [ -f "$niftyreg_path" ]; then
+    "$niftyreg_path" "${resample_options[@]}"
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error: NiftyReg failed with exit code $exit_code."
+        exit $exit_code
+    fi
+else
+    echo "Error: NiftyReg script not found at '$niftyreg_path'."
+    exit 1
+fi
+
+# Optional: Additional commands or post-processing steps
+
+echo "Resampling completed successfully."
