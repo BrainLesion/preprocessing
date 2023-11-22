4f2ddcb0ecdc646c0946fa2a4493ef10ed1091ad
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2023-11-22 17:17:26 +0100

    Corrected brain_extractor code

--- brainles_preprocessing/brain_extraction/brain_extractor.py
+++ brainles_preprocessing/brain_extraction/brain_extractor.py
@@ -8,7 +8,13 @@ from brainles_hd_bet import run_hd_bet
 
 class BrainExtractor:
     @abstractmethod
-    def extract(self, input_image, output_image, log_file, mode):
+    def extract(
+        self,
+        input_image,
+        output_image,
+        log_file,
+        mode,
+    ):
         pass
 
     def apply_mask(
@@ -30,11 +36,23 @@ class BrainExtractor:
 
 
 class HDBetExtractor(BrainExtractor):
-    def extract(self, input_image, output_image, log_file, mode):
+    def extract(
+        self,
+        input_image,
+        masked_image,
+        # TODO implement logging!
+        log_file,
+        mode="accurate",
+    ):
+        # GPU + accurate + TTA
+        """skullstrips images with HD-BET generates a skullstripped file and mask"""
         run_hd_bet(
             mri_fnames=[input_image],
-            output_fnames=[output_image],
-            mode="accurate",
+            output_fnames=[masked_image],
+            # device=0,
+            # TODO consider postprocessing
+            # postprocess=False,
+            mode=mode,
             device=0,
             postprocess=False,
             do_tta=True,
