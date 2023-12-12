551c7c50a268810e57189fede6281344916ab0a4
neuronflow <florian.kofler@tum.de>
2023-12-12 10:38:04 +0100

    new: with comments
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- brainles_preprocessing/registration/registrator.py
+++ brainles_preprocessing/registration/registrator.py
@@ -1,5 +1,5 @@
-# TODO add typing and docs
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class Registrator(ABC):
@@ -10,21 +10,47 @@ class Registrator(ABC):
     @abstractmethod
     def register(
         self,
-        fixed_image,
-        moving_image,
-        transformed_image,
-        matrix,
-        log_file,
-    ):
+        fixed_image: Any,
+        moving_image: Any,
+        transformed_image: Any,
+        matrix: Any,
+        log_file: str,
+    ) -> None:
+        """
+        Abstract method for registering images.
+
+        Args:
+            fixed_image (Any): The fixed image for registration.
+            moving_image (Any): The moving image to be registered.
+            transformed_image (Any): The resulting transformed image after registration.
+            matrix (Any): The transformation matrix applied during registration.
+            log_file (str): The path to the log file for recording registration details.
+
+        Returns:
+            None
+        """
         pass
 
     @abstractmethod
     def transform(
         self,
-        fixed_image,
-        moving_image,
-        transformed_image,
-        matrix,
-        log_file,
-    ):
+        fixed_image: Any,
+        moving_image: Any,
+        transformed_image: Any,
+        matrix: Any,
+        log_file: str,
+    ) -> None:
+        """
+        Abstract method for transforming images.
+
+        Args:
+            fixed_image (Any): The fixed image to be transformed.
+            moving_image (Any): The moving image to be transformed.
+            transformed_image (Any): The resulting transformed image.
+            matrix (Any): The transformation matrix applied during transformation.
+            log_file (str): The path to the log file for recording transformation details.
+
+        Returns:
+            None
+        """
         pass
