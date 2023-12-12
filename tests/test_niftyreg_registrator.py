da440a977dcb04b402166b9c8c228e319e1626f6
neuronflow <florian.kofler@tum.de>
2023-12-12 10:54:33 +0100

    first working test
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- tests/test_niftyreg_registrator.py
+++ tests/test_niftyreg_registrator.py
@@ -7,7 +7,7 @@ from auxiliary.turbopath import turbopath
 from brainles_preprocessing.registration.niftyreg import NiftyRegRegistrator
 
 
-class TestHDBetExtractor(unittest.TestCase):
+class TestNiftyRegRegistrator(unittest.TestCase):
     def setUp(self):
         test_data_dir = turbopath(__file__).parent + "/test_data"
         input_dir = test_data_dir + "/input"
@@ -36,23 +36,21 @@ class TestHDBetExtractor(unittest.TestCase):
             matrix_path=self.matrix,
             log_file_path=self.log_file,
         )
-        # TODO generate and also test for presence of log file
 
         self.assertTrue(
-            os.path.exists(self.transformed_image), "Masked image file was not created."
-        )
-        self.assertTrue(
-            os.path.exists(self.brain_mask_path),
-            "Brain mask image file was not created.",
+            os.path.exists(self.transformed_image),
+            "transformed file was not created.",
         )
 
-    def test_apply_mask_creates_output_file(self):
-        self.brain_extractor.apply_mask(
-            input_image_path=self.input_image_path,
-            mask_image_path=self.input_brain_mask_path,
-            masked_image_path=self.masked_again_image_path,
-        )
         self.assertTrue(
-            os.path.exists(self.masked_again_image_path),
-            "Output image file was not created in apply_mask.",
+            os.path.exists(self.matrix),
+            "matrix file was not created.",
         )
+
+        self.assertTrue(
+            os.path.exists(self.log_file),
+            "log file was not created.",
+        )
+
+
+# TODO also test transform
