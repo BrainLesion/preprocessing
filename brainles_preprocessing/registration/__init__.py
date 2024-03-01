cb92dbe0122415fdc3b0585a768cbcf5d9a8da6e
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2024-03-01 09:44:34 +0100

    Modify warning to install ants via brainles

--- brainles_preprocessing/registration/__init__.py
+++ brainles_preprocessing/registration/__init__.py
@@ -4,7 +4,7 @@ try:
     from .ANTs.ANTs import ANTsRegistrator
 except ImportError:
     warnings.warn(
-        "ANTS package not found. If you want to use it, please install it using 'pip install antspyx'"
+        "ANTS package not found. If you want to use it, please install it using 'pip install brainles_preprocessing[ants]'"
     )
 
 from .niftyreg.niftyreg import NiftyRegRegistrator
