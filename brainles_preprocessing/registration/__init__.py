d2e22274e5ca88660a04b33d721ce9f45c352cff
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2024-03-01 08:18:14 +0100

    Make ants an optional dependency

--- brainles_preprocessing/registration/__init__.py
+++ brainles_preprocessing/registration/__init__.py
@@ -1,2 +1,10 @@
-from .ANTs.ANTs import ANTsRegistrator
+import warnings
+
+try:
+    from .ANTs.ANTs import ANTsRegistrator
+except ImportError:
+    warnings.warn(
+        "ANTS package not found. If you want to use it, please install it using 'pip install antspyx'"
+    )
+
 from .niftyreg.niftyreg import NiftyRegRegistrator
