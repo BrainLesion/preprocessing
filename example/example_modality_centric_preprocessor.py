9d9dac2b4582ec632c637ac4b36b45e93227512b
neuronflow <florian.kofler@tum.de>
2024-03-30 14:26:50 +0100

    example
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- example/example_modality_centric_preprocessor.py
+++ example/example_modality_centric_preprocessor.py
@@ -6,7 +6,11 @@ from tqdm import tqdm
 from brainles_preprocessing.brain_extraction import HDBetExtractor
 from brainles_preprocessing.modality import Modality
 from brainles_preprocessing.preprocessor import Preprocessor
-from brainles_preprocessing.registration import ANTsRegistrator, NiftyRegRegistrator
+from brainles_preprocessing.registration import (
+    ANTsRegistrator,
+    NiftyRegRegistrator,
+    eRegRegistrator,
+)
 
 
 def preprocess(inputDir):
@@ -102,7 +106,8 @@ def preprocess(inputDir):
             moving_modalities=moving_modalities,
             # choose the registration backend you want to use
             # registrator=NiftyRegRegistrator(),
-            registrator=ANTsRegistrator(),
+            # registrator=ANTsRegistrator(),
+            registrator=eRegRegistrator(),
             brain_extractor=HDBetExtractor(),
             temp_folder="temporary_directory",
             limit_cuda_visible_devices="0",
