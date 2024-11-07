7ecb01803c22dd6026f9dbac8abe6db48f36f4f9
Marcel Rosier <marcel.rosier@gmx.de>
2024-11-07 11:44:43 +0100

    Remove irrelevant warning

--- brainles_preprocessing/registration/__init__.py
+++ brainles_preprocessing/registration/__init__.py
@@ -1,13 +1,6 @@
 import warnings
 
 try:
-    from .ANTs.ANTs import ANTsRegistrator
-except ImportError:
-    warnings.warn(
-        "ANTS package not found. If you want to use it, please install it using 'pip install brainles_preprocessing[ants]'"
-    )
-
-try:
     from .eReg.eReg import eRegRegistrator
 except ImportError:
     warnings.warn(
