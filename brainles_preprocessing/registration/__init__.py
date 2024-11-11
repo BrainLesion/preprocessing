00e94c97b7eafcd594ee5bd2c1497c2a0516d5a9
Marcel Rosier <marcel.rosier@gmx.de>
2024-11-11 09:40:52 +0100

    Read warning

--- brainles_preprocessing/registration/__init__.py
+++ brainles_preprocessing/registration/__init__.py
@@ -1,5 +1,13 @@
 import warnings
 
+
+try:
+    from .ANTs.ANTs import ANTsRegistrator
+except ImportError:
+    warnings.warn(
+        "ANTS package not found. If you want to use it, please install it using 'pip install antspyx'"
+    )
+
 try:
     from .eReg.eReg import eRegRegistrator
 except ImportError:
