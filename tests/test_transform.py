c6f05b8627f7396a1237c51c62bae9da052b1ab5
Marcel Rosier <marcel.rosier@gmx.de>
2025-07-10 13:14:28 +0200

    Add sanity test for forward transform

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -71,6 +71,45 @@ class TestTransform(unittest.TestCase):
             "interpolator": "linear",
         }
         mock_registrator.inverse_transform.assert_called_once_with(**expected_kwargs)
+        mock_registrator.transform.assert_not_called()
+
+    @patch("brainles_preprocessing.transform.Path.iterdir")
+    @patch("brainles_preprocessing.transform.Path.exists", return_value=True)
+    def test_transform_called_with_correct_args(self, mock_exists, mock_iterdir):
+        mock_registrator = MagicMock(spec=Registrator)
+        back = Transform(
+            transformations_dir=self.transform_dir,
+            registrator=mock_registrator,
+        )
+
+        # Mock Transformation files
+        fake_transforms = [
+            Path("/tmp/fake_transform1.txt"),
+            Path("/tmp/fake_transform2.txt"),
+        ]
+        mock_iterdir.return_value = fake_transforms
+
+        back.apply(
+            target_modality_name=self.target_modality_name,
+            target_modality_img=self.target_modality_img,
+            moving_image=self.moving_image,
+            output_img_path=self.output_img_path,
+            log_file_path=self.log_file_path,
+            interpolator="linear",
+            inverse=False,  # Test inverse transformation
+        )
+
+        # Check if inverse_transform was called with correct parameters
+        expected_kwargs = {
+            "fixed_image_path": self.target_modality_img,
+            "moving_image_path": self.moving_image,
+            "transformed_image_path": self.output_img_path,
+            "matrix_path": list(reversed(fake_transforms)),  # Inverse order
+            "log_file_path": str(self.log_file_path),
+            "interpolator": "linear",
+        }
+        mock_registrator.transform.assert_called_once_with(**expected_kwargs)
+        mock_registrator.inverse_transform.assert_not_called()
 
 
 if __name__ == "__main__":
